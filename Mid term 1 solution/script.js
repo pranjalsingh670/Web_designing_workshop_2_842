a9db0e5ea3226ec3a39ef30207f4bc48ef3dfe59
pranjalsingh670 <singh.pranjalabc2020@outlook.com>
2026-04-13 11:50:14 +0530

    mid term experiment

---
+++ Mid term 1 solution/script.js
@@ -0,0 +1,9 @@
+let num1 = parseFloat(prompt("Enter the first number:"));
+let num2 = parseFloat(prompt("Enter the second number:"));
+let sum = num1 + num2;
+let difference = num1 - num2;
+let product = num1 * num2;
+document.write("<h2> calculation result </h2>")
+document.write("Sum:" + sum);
+document.write("Difference:" + difference);
+document.write("Product: " + product);
